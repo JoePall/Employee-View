f1fd3ad341a5d4160728db38b745c3fbe8670438
Josiah Powell <josiahpowell@outlook.com>
2020-10-24 20:40:13 -0500

    Refactoring search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,28 +1,11 @@
 import React from "react";
+import SearchBox from "../../components/SearchBox"
 
 function Search(props) {
   
   return (
     <div className="container my-5 mx-auto">
-      <div className="input-group input-group-lg">
-        <input
-          type="text"
-          className="form-control rounded-pill-left"
-          aria-label="Employee search..."
-          aria-describedby="inputGroup-sizing-lg"
-          onChange={props.handleInputChange}
-          value={props.search}
-          name="search"
-          placeholder="Search..."
-          id="search"
-          />
-      <div className="input-group-append">
-        <button className="input-group-text btn-primary rounded-pill-right" id="inputGroup-sizing-lg">Search</button>
-      </div>
-    </div>
-    {
-      results
-    }
+      <SearchBox />
     </div>
   );
 }
